5941c2a822e792814ea13c31880a46c2525262a9
Tommy Simmons <42501816+Tommy8314@users.noreply.github.com>
2018-11-20 00:27:44 -0500

    Add files via upload

---
+++ SemesterProject/userHomePage.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Dummy Album</title>
+    <link rel="stylesheet" href="css/MusicDB.css" />
+    <link rel="stylesheet" href="css/Album.css" />
+    <script src="js/headFoot.js" type="text/JavaScript"></script>
+    <?php include('php/albumInfo.php'); ?>
+</head>
+
+<body>
+    <header>
+        <script>makeHeader();</script>
+    </header>
+    <main>
+        <section id="intro">
+            <h1>Username: Tommy</h1>
+            <img src="images/profilePic.png" class="main-image" alt="profile picture" title="profile picture" />
+        </section>
+        <section id="favoriteTracks">
+            <h2>Favorite Tracks</h2>
+            <ol>
+                <?php 
+                foreach($tracks as $track)
+                echo("<li>$track</li>");
+                ?>
+            </ol>
+        </section>
+        <section id="links-genre-tags">
+            <h2>Quick links to favorite genres</h2>
+            <ul>
+                <?php 
+                foreach($genres as $genre)
+                {
+                    echo("<li><a href='#'>$genre</a></li>");
+                }
+                ?>
+                <!--links to genre pages, if we decide to do those-->
+            </ul>
+        </section>
+        <section id="similar-albums">
+            <h2>Recently searched albums</h2>
+            <p>
+                <!--link these to their own album pages-->
+                <?php
+                for ($i = 0; $i < 4; $i++)
+                {
+                    echo("<a href='#'>");
+                        echo("<figure>");
+                            echo("<img src='images/album.jpg' class='main-image' alt='album cover' title='album cover' />");
+                        echo("</figure>");
+                    echo("</a>");
+                }
+                ?>
+            </p>
+        </section>
+    </main>
+    <footer>
+        <script>makeFooter();</script>
+    </footer>
+</body>
+
+</html>
